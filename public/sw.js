182dc3665e5d42758db8a9d903f83541f8272621
Oliver <schwimmakajeder@gmail.com>
2026-03-06 13:20:56 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,30 @@
+const CACHE_NAME = 'mob-crew-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/main.js',
+  '/style.css',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
 
+// Install-Event: Dateien in den Cache laden
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch-Event: Erst im Cache suchen, dann im Netzwerk
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
